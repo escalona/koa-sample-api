92789c4bc308f1fef380b9b2bea1e4576f575229
escalona <escalona.ap@gmail.com>
2017-01-18 13:52:10 -0800

    Move video object creation into model

--- controllers/videos-controller.js
+++ controllers/videos-controller.js
@@ -5,24 +5,7 @@ const VideoController = {
         let videoId = this.params.videoId;
 
         yield VideoModel.getVideoInfo(videoId).then((results) => {
-            let returnData = {};
-
-            if (results) {
-                returnData = {
-                    author: results.author,
-                    keywords: results.keywords,
-                    loaderUrl: results.loaderUrl,
-                    videoId: results.video_id,
-                    title: results.title,
-                    timestamp: results.timestamp,
-                    avgRating: results.avg_rating,
-                    thumbnailUrl: results.thumbnail_url,
-                    description: results.description,
-                    viewCount: results.view_count,
-                };
-            }
-
-            return this.body = returnData;
+            return this.body = results;
         }).catch((err) => {
             console.error(err.stack);
             return this.body = {error: true};
--- models/video-model.js
+++ models/video-model.js
@@ -3,7 +3,24 @@ import YoutubeService from '../services/youtube-service';
 const VideoModel = {
     getVideoInfo(videoId) {
         return YoutubeService.getVideoMetaData(videoId).then(function (results) {
-            return results;
+            let returnData = {};
+
+            if (results) {
+                returnData = {
+                    author: results.author,
+                    keywords: results.keywords,
+                    loaderUrl: results.loaderUrl,
+                    videoId: results.video_id,
+                    title: results.title,
+                    timestamp: results.timestamp,
+                    avgRating: results.avg_rating,
+                    thumbnailUrl: results.thumbnail_url,
+                    description: results.description,
+                    viewCount: results.view_count,
+                };
+            }
+
+            return returnData;
         });
     }
 };
